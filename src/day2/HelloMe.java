c810bed303781fb41821eca5250cc6ad1d5e3c3a
Chrissy121 <crys121@gmail.com>
2021-04-24 16:52:55 -0400

    added more work after next commit

--- src/day2/HelloMe.java
+++ src/day2/HelloMe.java
@@ -2,6 +2,10 @@ package day2;
 
 public class HelloMe {
     public static void main(String[] args) {
+
         System.out.println("Hello Me");
+        System.out.println("Adding GitHub Now");
+        System.out.println();
+
     }
 }
